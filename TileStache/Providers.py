f8c2dde7a4ffb8b1be6cc3ce97346f4236c294a9
Michal Migurski <migurski@geo.example.org>
2010-04-05 18:40:06 -0700

    Added copious docstrings

--- TileStache/Providers.py
+++ TileStache/Providers.py
@@ -1,9 +1,60 @@
+""" The provider bits of TileStache.
+
+A Provider the part of TileStache that actually renders imagery. A few default
+providers are found here, but it's possible to define your own and pull them into
+TileStache dynamically by class name.
+
+Built-in providers:
+- mapnik
+
+Example external provider, for JSON configuration file:
+
+    "layer-name": {
+        "provider": {"class": "Module.Classname", "kwargs": {"frob": "yes"}},
+        "projection": "spherical mercator"
+    }
+
+- The "class" value is split up into module and classname, and dynamically
+  included. If this doesn't work for some reason, TileStache will fail loudly
+  to let you know.
+- The "kwargs" value is fed to the class constructor as a dictionary of keyword
+  args. If your defined class doesn't accept any of these keyword arguments,
+  TileStache will throw an exception.
+
+The only method that a provider currently needs to implement is renderEnvelope(),
+with the following seven arguments:
+
+- width, height: in pixels
+- srs: projection as Proj4 string.
+  "+proj=longlat +ellps=WGS84 +datum=WGS84" is an example, 
+  see http://spatialreference.org for more.
+- xmin, ymin, xmax, ymax: coordinates of bounding box in projected coordinates.
+"""
+
 import mapnik
 import PIL.Image
 
 class Mapnik:
-
+    """ Built-in Mapnik provider. Renders map images from Mapnik XML files.
+    
+        This provider is identified by the name "mapnik" in the TileStache config.
+        
+        Additional arguments:
+        
+        - mapfile (required)
+            Local file path to Mapnik XML file.
+    
+        More information on Mapnik and Mapnik XML:
+        - http://mapnik.org
+        - http://trac.mapnik.org/wiki/XMLGettingStarted
+        - http://trac.mapnik.org/wiki/XMLConfigReference
+    """
     def __init__(self, layer, mapfile):
+        """ Initialize Mapnik provider with layer and mapfile.
+            
+            XML mapfile keyword arg comes from TileStache config,
+            and is an absolute path by the time it gets here.
+        """
         self.layer = layer
         self.mapfile = str(mapfile)
         self.mapnik = None
@@ -37,7 +88,9 @@ def getProviderByName(name):
     raise Exception('Unknown provider name: "%s"' % name)
 
 def loadProviderByClass(classpath):
-    """
+    """ Load external provider based on a class path.
+    
+        Example classpath: "Module.Submodule.Classname",
     """
     classpath = classpath.split('.')
     module = __import__( '.'.join(classpath[:-1]) )
