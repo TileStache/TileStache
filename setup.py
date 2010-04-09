72b80e4daaa06721fa615871847baecc407bc9b3
Michal Migurski <migurski@geo.example.org>
2010-04-09 09:06:25 -0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TileStache',
+      version='0.1.0',
+      description='A stylish alternative for caching your tiles.',
+      author='Michal Migurski',
+      author_email='mike@stamen.com',
+      url='http://tilestache.org',
+      requires=['ModestMaps'],
+      packages=['TileStache'],
+      download_url='http://tilestache.org/dist/TileStache-0.1.0.tar.gz',
+      license='BSD')
