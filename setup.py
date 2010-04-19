afb7e25253a550c61ee54915e31fab92c7f72e8a
migurski <migurski@ubuntu.(none)>
2010-04-18 22:23:30 -0700

    Kicked up a version number for future release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='TileStache',
-      version='0.1.1',
+      version='0.1.2',
       description='A stylish alternative for caching your tiles.',
       author='Michal Migurski',
       author_email='mike@stamen.com',
       url='http://tilestache.org',
       requires=['ModestMaps'],
       packages=['TileStache'],
-      download_url='http://tilestache.org/dist/TileStache-0.1.1.tar.gz',
+      download_url='http://tilestache.org/dist/TileStache-0.1.2.tar.gz',
       license='BSD')
