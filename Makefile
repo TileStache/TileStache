2058b3f003924bc7625c611bedf0a248de2c1ac0
migurski <migurski@ubuntu.(none)>
2010-04-22 09:53:12 -0700

    Incremented version number

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.1.5
+VERSION=0.1.6
 PACKAGE=TileStache-$(VERSION)
 TARBALL=$(PACKAGE).tar.gz
 DOCROOT=tilestache.org:public_html/tilestache/www
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='TileStache',
-      version='0.1.5',
+      version='0.1.6',
       description='A stylish alternative for caching your map tiles.',
       author='Michal Migurski',
       author_email='mike@stamen.com',
