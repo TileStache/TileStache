24d7a44ed0bcee16a2bc357c4d1ccd96e0bb4ee3
migurski <migurski@ubuntu.(none)>
2010-04-22 14:13:48 -0700

    version number elsewhere

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@
 
 from distutils.core import setup
 
+verion = '0.1.6'
+
 setup(name='TileStache',
-      version='0.1.6',
+      version=version,
       description='A stylish alternative for caching your map tiles.',
       author='Michal Migurski',
       author_email='mike@stamen.com',
@@ -14,5 +16,5 @@ setup(name='TileStache',
                 'TileStache.Goodies.Providers'],
       scripts=['scripts/tilestache-render.py', 'scripts/tilestache-seed.py'],
       data_files=[('share/tilestache', ['TileStache/Goodies/Providers/DejaVuSansMono-alphanumeric.ttf'])],
-      download_url='http://tilestache.org/download/TileStache-0.1.6.tar.gz',
+      download_url='http://tilestache.org/download/TileStache-%(version)s.tar.gz' % locals(),
       license='BSD')
