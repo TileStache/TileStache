0afa7c3dee1520b64b1e3db6b3f468e6e3b631f1
migurski <migurski@ubuntu.(none)>
2010-06-27 15:29:03 -0700

    Made PostGeoJSON provider return something JSON-like

--- TileStache/Goodies/Providers/PostGeoJSON.py
+++ TileStache/Goodies/Providers/PostGeoJSON.py
@@ -1,12 +1,27 @@
 """ Provider that returns GeoJSON data responses from PostGIS queries.
 """
 
+from json import dump
 from psycopg2 import connect
-
 from TileStache.Core import KnownUnknown
 
-class Provider:
+class SaveableResponse:
+    """ Wrapper class for JSON response that makes it behave like a PIL.Image object.
+    
+        TileStache.handleRequest() expects to be able to save one of these to a buffer.
+    """
+    def __init__(self, content):
+        self.content = content
 
+    def save(self, fp, format):
+        if format != 'JSON':
+            raise KnownUnknown('PostGeoJSON only makes .json tiles, not "%s"' % extension)
+
+        dump(self.content, fp)
+
+class Provider:
+    """
+    """
     def __init__(self, layer, dsn):
         self.layer = layer
         conn = connect('dbname=geodata user=postgres')
@@ -20,4 +35,4 @@ class Provider:
         return 'text/json', 'JSON'
 
     def renderTile(self, width, height, srs, coord):
-        pass
+        return SaveableResponse({'hello': 'world'})
