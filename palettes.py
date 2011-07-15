75213596f95595997c9a81ac47b7ba8aa36a567b
Michal Migurski <mike@stamen.com>
2011-07-15 11:23:48 -0700

    Removed numpy dependency, hopefully without too much performance impact

--- palettes.py
+++ palettes.py
@@ -1,6 +1,6 @@
 from struct import unpack, pack
-from numpy import zeros, uint8, sqrt as nsqrt
-from math import log, ceil
+from math import log, ceil, sqrt
+from operator import add
 
 try:
     from PIL import Image, ImagePalette
@@ -8,49 +8,53 @@ except:
     import Image, ImagePalette
 
 def load_palette(filename):
-    """ Load colors from a Photoshop .act file, return an Nx3 uint8 palette array.
+    """ Load colors from a Photoshop .act file, return a palette array.
+    
+        Return array is structured as: [ (r, g, b), (r, g, b), ... ]
     """
     bytes = open(filename, 'r').read()
-    count, transparent = unpack('!HH', bytes[768:772])
-    palette = zeros((count, 3), dtype=uint8)
+    count, t_index = unpack('!HH', bytes[768:768+4])
+    palette = []
     
     for offset in range(0, count):
-        if offset == transparent:
+        if offset == t_index:
             rgb = 0xff, 0x99, 0x00
         else:
-            rgb = unpack('!BBB', bytes[offset*3:offset*3+3])
-
-        palette[offset,:] = rgb
+            rgb = unpack('!BBB', bytes[offset*3:(offset + 1)*3])
+        
+        palette.append(rgb)
     
-    return palette, transparent
+    return palette, t_index
 
-def palette_color(r, g, b, palette):
+def palette_color(r, g, b, palette, t_index):
     """ Return best palette match index.
 
         Find the closest color in the palette based on dumb euclidian distance,
         assign its index in the palette to a mapping from 24-bit color tuples.
     """
-    distances = (palette[:,0] - r)**2 + (palette[:,1] - g)**2 + (palette[:,2] - b)**2
-    distances = list(nsqrt(distances))
+    distances = [(r - _r)**2 + (g - _g)**2 + (b - _b)**2 for (_r, _g, _b) in palette]
+    distances = map(sqrt, distances)
+    
+    if t_index is not None:
+        distances = distances[:t_index] + distances[t_index+1:]
     
     return distances.index(min(distances))
 
-def palettize(image, palette, t_index):
+def palettize(image, palette, t_index=None):
     """ Apply an Nx3 palette array to an image, return it and calculate bit depth.
     """
     image = image.convert('RGBA')
     pixels = image.tostring()
-    palette = palette.astype(float)
-    t_index = (t_index <= 0xff) and pack('!B', t_index) or None
+    t_value = (t_index <= 0xff) and pack('!B', t_index) or None
     mapping = {}
     indexes = []
     
     for offset in range(0, len(pixels), 4):
         r, g, b, a = unpack('!BBBB', pixels[offset:offset+4])
         
-        if a < 0x80 and t_index is not None:
+        if a < 0x80 and t_value is not None:
             # Sufficiently transparent
-            indexes.append(t_index)
+            indexes.append(t_value)
             continue
         
         try:
@@ -58,7 +62,7 @@ def palettize(image, palette, t_index):
 
         except KeyError:
             # Never seen this color
-            mapping[(r, g, b)] = pack('!B', palette_color(r, g, b, palette))
+            mapping[(r, g, b)] = pack('!B', palette_color(r, g, b, palette, t_index))
         
         else:
             continue
@@ -66,21 +70,21 @@ def palettize(image, palette, t_index):
         indexes.append(mapping[(r, g, b)])
 
     output = Image.fromstring('P', image.size, ''.join(indexes))
-    bits = int(ceil(log(palette.shape[0]) / log(2)))
+    bits = int(ceil(log(len(palette)) / log(2)))
     
-    palette = [unpack('!B', char)[0] for char in palette.astype(uint8).data]
-    palette += [0] * (768 - len(palette))
+    palette += [(0, 0, 0)] * (256 - len(palette))
+    palette = reduce(add, palette)
     output.putpalette(palette)
     
     return output, bits
 
 image = Image.open('/Users/migurski/Pictures/stupid bicycle.jpg')
-palette, transparent = load_palette('/tmp/bicycle.act')
+palette, t_index = load_palette('/tmp/bicycle.act')
 
 image = Image.open('/tmp/inundation_b.png')
-palette, transparent = load_palette('/tmp/inundation_b.act')
+palette, t_index = load_palette('/tmp/inundation_b.act')
 
-output, bits = palettize(image, palette, transparent)
-kwargs = dict(optimize=True, transparency=transparent, bits=bits)
+output, bits = palettize(image, palette, t_index)
+kwargs = dict(optimize=True, transparency=t_index, bits=bits)
 
 output.save('/tmp/inundation_b-P.png', **kwargs)
