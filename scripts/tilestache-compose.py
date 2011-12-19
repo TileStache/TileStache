b8816e7e6bdf2be73e18cd72b1b98abe99944a22
Michal Migurski <mike@stamen.com>
2011-12-18 18:35:47 -0800

    Added configuration and layer flags to tilestache-compose.py

--- scripts/tilestache-compose.py
+++ scripts/tilestache-compose.py
@@ -58,61 +58,77 @@ class Provider (ModestMaps.Providers.IMapProvider):
 class BadComposure(Exception):
     pass
 
-parser = OptionParser(usage="""compose.py [options] file
+parser = OptionParser(usage="""tilestache-compose.py [options] file
 
 There are three ways to set a map coverage area.
 
 1) Center, zoom, and dimensions: create a map of the specified size,
    centered on a given geographical point at a given zoom level:
 
-   python compose.py -p OPENSTREETMAP -d 800 800 -c 37.8 -122.3 -z 11 out.jpg
+   tilestache-compose.py -c config.json -l layer-name -d 800 800 -n 37.8 -122.3 -z 11 out.jpg
 
 2) Extent and dimensions: create a map of the specified size that
    adequately covers the given geographical extent:
 
-   python compose.py -p MICROSOFT_ROAD -d 800 800 -e 36.9 -123.5 38.9 -121.2 out.png
+   tilestache-compose.py -c config.json -l layer-name -d 800 800 -e 36.9 -123.5 38.9 -121.2 out.png
 
 3) Extent and zoom: create a map at the given zoom level that covers
    the precise geographical extent, at whatever pixel size is necessary:
    
-   python compose.py -p BLUE_MARBLE -e 36.9 -123.5 38.9 -121.2 -z 9 out.jpg""")
+   tilestache-compose.py -c config.json -l layer-name -e 36.9 -123.5 38.9 -121.2 -z 9 out.jpg""")
 
 defaults = dict(center=(37.8044, -122.2712), zoom=14, dimensions=(900, 600), verbose=True)
 
 parser.set_defaults(**defaults)
 
-parser.add_option('-v', '--verbose', dest='verbose',
-                  help='Make a bunch of noise',
-                  action='store_true')
+parser.add_option('-c', '--config', dest='config',
+                  help='Path to configuration file.')
 
-parser.add_option('-c', '--center', dest='center', nargs=2,
-                  help='Center. lat, lon, e.g.: 37.804 -122.263', type='float',
+parser.add_option('-l', '--layer', dest='layer',
+                  help='Layer name from configuration.')
+
+parser.add_option('-n', '--center', dest='center', nargs=2,
+                  help='Geographic center of map. Default %.4f, %.4f.' % defaults['center'], type='float',
                   action='store')
 
 parser.add_option('-e', '--extent', dest='extent', nargs=4,
-                  help='Geographical extent. Two lat, lon pairs', type='float',
+                  help='Geographic extent of map. Two lat, lon pairs', type='float',
                   action='store')
 
 parser.add_option('-z', '--zoom', dest='zoom',
-                  help='Zoom level', type='int',
+                  help='Zoom level. Default %(zoom)d.' % defaults, type='int',
                   action='store')
 
 parser.add_option('-d', '--dimensions', dest='dimensions', nargs=2,
-                  help='Pixel dimensions of image', type='int',
+                  help='Pixel width, height of output image. Default %d, %d.' % defaults['dimensions'], type='int',
                   action='store')
+
+parser.add_option('-v', '--verbose', dest='verbose',
+                  help='Make a bunch of noise.',
+                  action='store_true')
 
 if __name__ == '__main__':
 
     (options, args) = parser.parse_args()
     
     try:
+        if options.config is None:
+            raise TileStache.Core.KnownUnknown('Missing required configuration (--config) parameter.')
+
+        if options.layer is None:
+            raise TileStache.Core.KnownUnknown('Missing required layer (--layer) parameter.')
+
+        config = TileStache.parseConfigfile(options.config)
+
+        if options.layer not in config.layers:
+            raise TileStache.Core.KnownUnknown('"%s" is not a layer I know about. Here are some that I do know about: %s.' % (options.layer, ', '.join(sorted(config.layers.keys()))))
+
+        provider = Provider(config.layers[options.layer])
+        
         try:
             outfile = args[0]
         except IndexError:
             raise BadComposure('Error: Missing output file.')
-        
-        config = TileStache.parseConfigfile('tilestache.cfg')
-        provider = Provider(config.layers['osm'])
         
         if options.center and options.extent:
             raise BadComposure("Error: bad map coverage, center and extent can't both be set.")
