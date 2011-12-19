588baa2c0002c6b1fc6564d639af5e6d243574af
Michal Migurski <mike@stamen.com>
2011-12-18 16:05:12 -0800

    Fleshed out threading behavior in Provider class

--- scripts/tilestache-compose.py
+++ scripts/tilestache-compose.py
@@ -1,5 +1,6 @@
 from sys import stderr
 from tempfile import mkstemp
+from thread import allocate_lock
 from os import close, write, unlink
 from os.path import abspath
 
@@ -10,10 +11,21 @@ config = TileStache.parseConfigfile('tilestache.cfg')
 
 class Provider (ModestMaps.Providers.IMapProvider):
 
-    def __init__(self, layer):
+    def __init__(self, layer, use_threads=None):
         self.projection = layer.projection
         self.layer = layer
         self.files = []
+
+        self.lock = allocate_lock()
+        
+        #
+        # It's possible that Mapnik is not thread-safe, best to be cautious.
+        # Otherwise, allow the constructor to specify whether to use threads.
+        #
+        if use_threads is None:
+            self.threadsafe = self.layer.provider is not TileStache.Providers.Mapnik
+        else:
+            self.threadsafe = use_threads
 
     def tileWidth(self):
         return 256
@@ -22,23 +34,26 @@ class Provider (ModestMaps.Providers.IMapProvider):
         return 256
 
     def getTileUrls(self, coord):
-        print >> stderr, coord, '...',
-
-        mime_type, tile_data = TileStache.getTile(self.layer, coord, 'png')
-        
-        handle, filename = mkstemp(prefix='tile-', suffix='.png')
-        write(handle, tile_data)
-        close(handle)
-        
-        self.files.append(filename)
-        
-        print >> stderr, filename
-
-        return ('file://' + abspath(filename), )
+        """ Return tile URLs that start with file://, by first retrieving them.
+        """
+        if self.threadsafe or self.lock.acquire():
+            mime_type, tile_data = TileStache.getTile(self.layer, coord, 'png')
+            
+            handle, filename = mkstemp(prefix='tilestache-compose-', suffix='.png')
+            write(handle, tile_data)
+            close(handle)
+            
+            self.files.append(filename)
+            
+            if self.lock.locked():
+                self.lock.release()
+    
+            return ('file://' + abspath(filename), )
     
     def __del__(self):
+        """ Delete any tile that was saved in self.getTileUrls().
+        """
         for filename in self.files:
-            print >> stderr, 'no more', filename
             unlink(filename)
 
 provider = Provider(config.layers['osm'])
