2058f175b34a458b5c260040fd0b5c853e007570
Thomas Hirsch <thomas.hirsch@statkart.no>
2012-03-14 10:00:28 +0100

    type-o

--- TileStache/Config.py
+++ TileStache/Config.py
@@ -418,7 +418,7 @@ def _parseConfigfileLayer(layer_dict, config, dirpath):
             provider_kwargs['properties'] = provider_dict.get('properties', None)
             provider_kwargs['projected'] = bool(provider_dict.get('projected', False))
             provider_kwargs['verbose'] = bool(provider_dict.get('verbose', False))
-            provider_kwargs['precision'] = bool(provider_dict.get('precision', 6))
+            provider_kwargs['precision'] = int(provider_dict.get('precision', 6))
             
             if 'spacing' in provider_dict:
                 provider_kwargs['spacing'] = float(provider_dict.get('spacing', 0.0))
