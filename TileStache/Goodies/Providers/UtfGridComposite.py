a8ca59a86868c42d0e6faf24e382e9e89f064600
Tom Nightingale <tom@tnightingale.com>
2012-04-30 17:22:39 -0700

    unicode/json encoding for grid keys < 127

--- TileStache/Goodies/Providers/UtfGridComposite.py
+++ TileStache/Goodies/Providers/UtfGridComposite.py
@@ -168,7 +168,7 @@ class Provider:
 		
 		for i in xrange(gridSize):
 			line = finalGrid[i]
-			result += "\"" + line + "\""
+			result += json.dumps(line)
 			if i < gridSize - 1:
 				result += ","
 		
@@ -183,6 +183,8 @@ class Provider:
 			id = id + 1
 		if id >= 92:
 			id = id + 1
+		if id > 127:
+			return unichr(id)
 		return chr(id)
 
 	def decodeId( self, id ):
