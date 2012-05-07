7fd35bb0ebf4bb72fc9315b59494eff087a5172d
Michal Migurski <migurski@geo.example.org>
2012-05-06 23:16:47 -0700

    Bumped to 1.32.1 with output of Mapnik Grid as pure UTF-8 instead of ASCII-encoded for size

--- TileStache/Mapnik.py
+++ TileStache/Mapnik.py
@@ -235,7 +235,8 @@ class SaveableResponse:
         if format != 'JSON':
             raise KnownUnknown('MapnikGrid only saves .json tiles, not "%s"' % format)
 
-        json.dump(self.content, out)
+        bytes = json.dumps(self.content, ensure_ascii=False).encode('utf-8')
+        out.write(bytes)
     
     def crop(self, bbox):
         """ Return a cropped grid response.
