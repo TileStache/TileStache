f986b2dcbbc8a76e36c5f5f3a08084cc330ced93
Yuval Adam <yuv.adm@gmail.com>
2012-10-14 15:55:45 +0200

    Redis works. Sanity restored.

--- TileStache/Redis.py
+++ TileStache/Redis.py
@@ -27,7 +27,13 @@ def tile_key(layer, coord, format, rev=0):
 
 class Cache:
     def __init__(self, url='redis://localhost'):
-        self.r = redis.from_url(url)
+        self.url = url
+
+    @property
+    def mem(self):
+        if getattr(self, 'r', None) is None:
+            self.r = redis.from_url(self.url)
+        return self.r
 
     def lock(self, layer, coord, format):
         # IMPLEMENT LOCKING!!!
@@ -41,27 +47,24 @@ class Cache:
         #     _sleep(.2)
 
         # self.mem.setex(key + '-lock', layer.stale_lock_timeout, 'locked.')
-        logging.debug('lock: ')
         return
 
     def unlock(self, layer, coord, format):
         key = tile_key(layer, coord, format)
-        logging.debug('unlock: ' + key)
-        self.r.delete(key + '-lock')
+        self.mem.delete(key + '-lock')
 
     def remove(self, layer, coord, format):
         key = tile_key(layer, coord, format)
-        logging.debug('remove: ' + key)
-        self.r.delete(key)
+        self.mem.delete(key)
 
     def read(self, layer, coord, format):
         key = tile_key(layer, coord, format)
-        logging.debug('read: ' + key)
-        value = self.r.get(key)
-        logging.debug('value: ' + value)
+        value = self.mem.get(key)
         return value
 
     def save(self, body, layer, coord, format):
         key = tile_key(layer, coord, format)
-        logging.debug('save: ' + key)
-        self.r.setex(key, layer.cache_lifespan or 0, body)
+        if layer.cache_lifespan:
+            self.mem.setex(key, layer.cache_lifespan, body)
+        else:
+            self.mem.set(key, body)
