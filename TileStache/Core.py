3d2e9bc1eba51a5d841df5146880243c842b536a
Lee Shepherd <bigwebguy@gmail.com>
2013-03-07 21:59:55 -0800

    force preview to 256x256 tile dimension

--- TileStache/Core.py
+++ TileStache/Core.py
@@ -598,6 +598,11 @@ def _preview(layer):
             margin: 0;
             padding: 0;
         }
+
+        #map img {
+            width: 256px;
+            height: 256px;
+        }
     </style>
 </head>
 <body>
