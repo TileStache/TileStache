576f3afffeed6915a8d26ffa15feb3ec2079df29
Mike Migurski <migurski@benzene.osm.osuosl.org>
2013-03-12 19:21:14 -0500

    Added queries parameter to VecTiles Provider

--- TileStache/Goodies/VecTiles/server.py
+++ TileStache/Goodies/VecTiles/server.py
@@ -1,4 +1,7 @@
 from math import pi
+from urlparse import urljoin, urlparse
+from urllib import urlopen
+from os.path import exists
 
 try:
     from psycopg2.extras import RealDictCursor
@@ -15,25 +18,81 @@ from . import mvt, geojson
 tolerances = [6378137 * 2 * pi / (2 ** (zoom + 8)) for zoom in range(20)]
     
 class Provider:
+    ''' VecTiles provider for PostGIS data sources.
+    
+        Parameters:
+        
+          queries:
+            List of Postgres queries, one for each zoom level. The last query
+            in the list is repeated for higher zoom levels, and null queries
+            indicate an empty response. Query must use "geometry" for a column
+            name, and must be in spherical mercator (900913) projection.
+            A query can additionally be a file name or URL, interpreted
+            relative to the location of the TileStache config file.
+        
+        Sample configuration, for a layer with no results at zooms 0-9, basic
+        selection of lines with names and highway tags for zoom 10, a remote
+        URL containing a query for zoom 11, and a local file for zooms 12+:
+        
+          "provider":
+          {
+            "class": "TileStache.Goodies.VecTiles:Provider",
+            "kwargs":
+            {
+              "queries":
+              [
+                null, null, null, null, null,
+                null, null, null, null, null,
+                "SELECT way AS geometry, highway, name FROM planet_osm_line -- zoom 10+ ",
+                "http://example.com/query-z11.pgsql",
+                "query-z12-plus.pgsql"
+              ]
+            }
+          }
     '''
-    '''
-    def __init__(self, layer):
+    def __init__(self, layer, queries):
         '''
         '''
         self.layer = layer
         self.db = connect(host='localhost', user='gis', password='gis', database='gis').cursor(cursor_factory=RealDictCursor)
         
+        self.queries = []
+        
+        for query in queries:
+            if query is None:
+                self.queries.append(None)
+                continue
+        
+            #
+            # might be a file or URL?
+            #
+            url = urljoin(layer.config.dirpath, query)
+            scheme, h, path, p, q, f = urlparse(url)
+            
+            if scheme in ('file', '') and exists(path):
+                query = open(path).read()
+            
+            elif scheme == 'http' and ' ' not in url:
+                query = urlopen(url).read()
+        
+            self.queries.append(query)
+        
     def renderTile(self, width, height, srs, coord):
         '''
         '''
+        try:
+            query = self.queries[coord.zoom]
+        except IndexError:
+            query = self.queries[-1]
+
+        if not query:
+            return EmptyResponse()
+        
         ll = self.layer.projection.coordinateProj(coord.down())
         ur = self.layer.projection.coordinateProj(coord.right())
-
         bbox = 'MakeBox2D(MakePoint(%.2f, %.2f), MakePoint(%.2f, %.2f))' % (ll.x, ll.y, ur.x, ur.y)
-        query = 'SELECT way AS geometry, highway, name FROM planet_osm_line'
-        tolerance = tolerances[coord.zoom]
         
-        return Response(self.db, query, bbox, tolerance)
+        return Response(self.db, query, bbox, tolerances[coord.zoom])
 
     def getTypeByExtension(self, extension):
         '''
@@ -50,14 +109,14 @@ class Provider:
 class Response:
     '''
     '''
-    def __init__(self, db, query, bbox, tolerance):
+    def __init__(self, db, subquery, bbox, tolerance):
         '''
         '''
         self.db = db
         
         self.query = {
-            'JSON': build_query(query, bbox, tolerance, True),
-            'MVT': build_query(query, bbox, tolerance, False)
+            'JSON': build_query(subquery, bbox, tolerance, True),
+            'MVT': build_query(subquery, bbox, tolerance, False)
             }
     
     def save(self, out, format):
@@ -82,7 +141,22 @@ class Response:
         else:
             raise ValueError(format)
 
-def build_query(query, bbox, tolerance, geo):
+class EmptyResponse:
+    ''' Simple empty response renders valid MVT or GeoJSON with no features.
+    '''
+    def save(self, out, format):
+        '''
+        '''
+        if format == 'MVT':
+            mvt.encode(out, [])
+        
+        elif format == 'JSON':
+            geojson.encode(out, [])
+        
+        else:
+            raise ValueError(format)
+
+def build_query(subquery, bbox, tolerance, geo):
     '''
     '''
     bbox = 'SetSRID(%s, 900913)' % bbox
@@ -94,7 +168,9 @@ def build_query(query, bbox, tolerance, geo):
     
     return '''SELECT q.*,
                      AsBinary(%(geom)s) AS geometry
-              FROM (%(query)s) AS q
+              FROM (
+                %(subquery)s
+                ) AS q
               WHERE q.geometry && %(bbox)s
                 AND Intersects(q.geometry, %(bbox)s)''' \
             % locals()
