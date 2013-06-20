eb0d9cafbcb42bffc17743cf31892c41c560c494
Michal Migurski <migurski@tilestache.(none)>
2013-06-20 06:31:23 +0000

    Added basic tests for vectiles TopoJSON

--- tests/vectiles_tests.py
+++ tests/vectiles_tests.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from math import hypot
 import json
 
 from osgeo import ogr
@@ -9,6 +10,27 @@ from . import utils
 # Note these tests rely on the fact that Travis CI created a postgis db.
 # If you want to run them locally, create a similar PostGIS database.
 # Look at .travis.yml for details.
+
+def get_topo_transform(topojson):
+    '''
+    '''
+    def xform((x, y)):
+        lon = topojson['transform']['scale'][0] * x + topojson['transform']['translate'][0]
+        lat = topojson['transform']['scale'][1] * y + topojson['transform']['translate'][1]
+        
+        return lon, lat
+    
+    return xform
+
+def topojson_dediff(points):
+    '''
+    '''
+    out = [points[0]]
+    
+    for (x, y) in points[1:]:
+        out.append((out[-1][0] + x, out[-1][1] + y))
+    
+    return out
 
 class PostGISVectorTestBase(object):
     '''
@@ -109,7 +131,7 @@ class VectorProviderTest(PostGISVectorTestBase, TestCase):
         tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "json", 0, 0, 1)
         geojson_result = json.loads(tile_content)
 
-        self.assertEqual(tile_mimetype, "text/json")
+        self.assertTrue(tile_mimetype.endswith('/json'))
         self.assertEqual(geojson_result['type'], 'FeatureCollection')
         self.assertEqual(len(geojson_result['features']), 2)
 
@@ -135,7 +157,7 @@ class VectorProviderTest(PostGISVectorTestBase, TestCase):
         tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "json", 0, 1, 1)
         geojson_result = json.loads(tile_content)
 
-        self.assertEqual(tile_mimetype, "text/json")
+        self.assertTrue(tile_mimetype.endswith('/json'))
         self.assertEqual(geojson_result['type'], 'FeatureCollection')
         self.assertEqual(len(geojson_result['features']), 1)
         self.assertTrue('Berlin' in geojson_result['features'][0]['properties']['name'])
@@ -153,7 +175,7 @@ class VectorProviderTest(PostGISVectorTestBase, TestCase):
         # we should have a line that clips at 0...
 
         tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "json", 0, 0, 0)
-        self.assertEqual(tile_mimetype, "text/json")
+        self.assertTrue(tile_mimetype.endswith('/json'))
         geojson_result = json.loads(tile_content)
         west_hemisphere_geometry = asShape(geojson_result['features'][0]['geometry'])
         expected_geometry = LineString([(-180, 32), (180, 32)])
@@ -176,7 +198,7 @@ class VectorProviderTest(PostGISVectorTestBase, TestCase):
         self.insertTestRow(geom.wkt)
         
         tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "json", 0, 0, 0)
-        self.assertEqual(tile_mimetype, "text/json")
+        self.assertTrue(tile_mimetype.endswith('/json'))
         geojson_result = json.loads(tile_content)
         
         result_geom = asShape(geojson_result['features'][0]['geometry'])
@@ -203,3 +225,141 @@ class VectorProviderTest(PostGISVectorTestBase, TestCase):
         
         self.assertTrue(result_geom.difference(expected_geom.buffer(0.001)).is_empty)
         self.assertTrue(expected_geom.difference(result_geom.buffer(0.001)).is_empty)
+    
+    def test_points_topojson(self):
+        '''
+        Create 3 points (2 on west, 1 on east hemisphere) and retrieve as topojson.
+        2 points should be returned in western hemisphere and 1 on eastern at zoom level 1
+        (clip on)
+        '''
+        
+        self.defineGeometry('POINT')
+
+        point_sf = Point(-122.4183, 37.7750)
+        point_berlin = Point(13.4127, 52.5233)
+        point_lima = Point(-77.0283, 12.0433)
+
+        self.insertTestRow(point_sf.wkt, 'San Francisco')
+        self.insertTestRow(point_berlin.wkt, 'Berlin')
+        self.insertTestRow(point_lima.wkt, 'Lima')
+
+        ########
+        # northwest quadrant should return San Francisco and Lima
+
+        tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "topojson", 0, 0, 1)
+        topojson_result = json.loads(tile_content)
+
+        self.assertTrue(tile_mimetype.endswith('/json'))
+        print topojson_result
+        self.assertEqual(topojson_result['type'], 'Topology')
+        self.assertEqual(len(topojson_result['objects']['vectile']['geometries']), 2)
+
+        cities = []
+
+        # Make sure that the right cities have been returned and that the geometries match
+        
+        topojson_xform = get_topo_transform(topojson_result)
+
+        for feature in topojson_result['objects']['vectile']['geometries']:
+            lon, lat = topojson_xform(feature['coordinates'])
+            
+            if feature['properties']['name'] == 'San Francisco':
+                cities.append(feature['properties']['name'])
+                self.assertTrue(hypot(point_sf.x - lon, point_sf.y - lat) < 1)
+
+            elif feature['properties']['name'] == 'Lima':
+                cities.append(feature['properties']['name'])
+                print feature['coordinates']
+                self.assertTrue(hypot(point_lima.x - lon, point_lima.y - lat) < 1)
+
+        self.assertTrue('San Francisco' in cities)
+        self.assertTrue('Lima' in cities)
+
+        ##########
+        # northeast quadrant should return Berlin
+
+        tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "topojson", 0, 1, 1)
+        topojson_result = json.loads(tile_content)
+
+        self.assertTrue(tile_mimetype.endswith('/json'))
+        self.assertEqual(topojson_result['type'], 'Topology')
+        self.assertEqual(len(topojson_result['objects']['vectile']['geometries']), 1)
+        self.assertTrue('Berlin' in topojson_result['objects']['vectile']['geometries'][0]['properties']['name'])
+
+
+    def test_linestring_topojson(self):
+        '''Create a line that goes from west to east (clip on)'''
+        
+        self.defineGeometry('LINESTRING')
+
+        geom = LineString( [(-180, 32), (180, 32)] )
+
+        self.insertTestRow(geom.wkt)
+
+        # we should have a line that clips at 0...
+
+        tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "topojson", 0, 0, 0)
+        self.assertTrue(tile_mimetype.endswith('/json'))
+        topojson_result = json.loads(tile_content)
+        topojson_xform = get_topo_transform(topojson_result)
+        
+        parts = [topojson_result['arcs'][arc] for arc in topojson_result['objects']['vectile']['geometries'][0]['arcs']]
+        parts = [map(topojson_xform, topojson_dediff(part)) for part in parts]
+        
+        west_hemisphere_geometry = LineString(*parts)
+        
+        # Close enough?
+        self.assertTrue(abs(west_hemisphere_geometry.coords[0][0] + 180) < 2)
+        self.assertTrue(abs(west_hemisphere_geometry.coords[1][0] - 180) < 2)
+        self.assertTrue(abs(west_hemisphere_geometry.coords[0][1] - 32) < 2)
+        self.assertTrue(abs(west_hemisphere_geometry.coords[1][1] - 32) < 2)
+
+
+    def test_polygon_topojson(self):
+        '''
+        Create a polygon to cover the world and make sure it is "similar" (clip on)
+        '''
+        
+        self.defineGeometry('POLYGON')
+
+        geom = Polygon( [(-180, -85.0511),
+                         ( 180, -85.0511),
+                         ( 180, 85.0511), 
+                         (-180, 85.0511), 
+                         (-180, -85.0511)])
+
+        self.insertTestRow(geom.wkt)
+        
+        tile_mimetype, tile_content = utils.request(self.config_file_content, "vectile_test", "topojson", 0, 0, 0)
+        self.assertTrue(tile_mimetype.endswith('/json'))
+        topojson_result = json.loads(tile_content)
+        topojson_xform = get_topo_transform(topojson_result)
+        
+        parts = [topojson_result['arcs'][arc[0]] for arc in topojson_result['objects']['vectile']['geometries'][0]['arcs']]
+        parts = [map(topojson_xform, topojson_dediff(part)) for part in parts]
+        
+        result_geom = Polygon(*parts)
+        expected_geom = Polygon( [(-180, -85.0511), (180, -85.0511), (180, 85.0511), (-180, 85.0511), (-180, -85.0511)])
+
+        # What is going on here is a bit unorthodox, but let me explain. The clipping
+        # code inside TileStache relies on GEOS Intersection alongside some TileStache code
+        # that creates a clipping geometry based on the tile perimeter. The tile perimeter
+        # is made out of 17 (x,y) coordinates and not a box. Hence, the GEOS::Intersection
+        # os that perimeter with the geometry of the vector we get back from the data provider
+        # can end with extra vertices. Although it is the right shape, we cannot do a straight
+        # comparisson because the expected geometry and the returned geometry *may* have extra
+        # vertices. Simplify() will not do much because the distance of the vertices can clearly
+        # be bigger than the tolerance. 
+        #
+        # To add to this, because of double precision, the vertices may not be exact.
+        # An optional way to find out if two shapes are close enough, is to buffer the two features
+        # by just a little bit and then subtract each other like so:
+        #
+        #             geometry1.difference(geometry2) == empty set?
+        #             geometry2.difference(geometry1) == empty set?
+        # 
+        # If both geometries are empty, then they are similar. Hence what you see below
+        
+        # Close enough?
+        self.assertTrue(result_geom.difference(expected_geom.buffer(1)).is_empty)
+        self.assertTrue(expected_geom.difference(result_geom.buffer(1)).is_empty)
