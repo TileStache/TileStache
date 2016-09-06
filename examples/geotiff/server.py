5d27f57b97746bedd17edfb0b1afd73a4ca15481
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2016-09-06 10:49:24 -0400

    Updated script documentation

--- examples/geotiff/server.py
+++ examples/geotiff/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-"""tilestache-server.py will serve your cache.
+"""server.py will serve your cache.
 
-This script is intended to be run directly from the command line.
+This script is intended to be run directly from the command line from its current location.
 
 It is intended for direct use only during development or for debugging TileStache.
 
@@ -11,18 +11,15 @@ http://tilestache.org/doc/#serving-tiles
 
 To use this built-in server, install werkzeug and then run tilestache-server.py:
 
-    tilestache-server.py
+    server.py
 
-By default the script looks for a config file named tilestache.cfg in the current directory and then serves tiles on http://127.0.0.1:8080/.
+By default the script serves tiles on http://127.0.0.1:8080/.
 
 You can then open your browser and view a url like:
 
-    http://localhost:8080/osm/0/0/0.png
+    http://localhost:8080/geotiff/0/0/0.png
 
-The above layer of 'osm' (defined in the tilestache.cfg) will display an OpenStreetMap
-tile proxied from http://tile.osm.org/0/0/0.png
-
-Check tilestache-server.py --help to change these defaults.
+Check server.py --help to change these defaults.
 """
 
 import json
