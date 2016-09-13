52d64a4450cf23c913f536d88a1ebf7e222acb04
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2016-09-13 15:38:46 -0400

    Added test to verify parsing of XML string correctly

--- tests/config_tests.py
+++ tests/config_tests.py
@@ -27,3 +27,42 @@ class ConfigTests(TestCase):
         self.assertEqual(config.cache.revision, 4)
         self.assertTrue(config.layers['memcache_osm'])
         self.assertTrue(isinstance(config.layers['memcache_osm'], Core.Layer))
+
+    def test_config_mapnik_string(self):
+        '''Read configuration and pass mapnik configuration as string'''
+
+        mapnik_config = '''
+            <?xml version="1.0"?>
+            <Map font-directory="./fonts" srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over">
+            <Layer name="raster-layer" srs="+proj=cea +lon_0=-117.333333333333 +lat_ts=33.75 +x_0=0 +y_0=0 +datum=NAD27 +units=m +no_defs " status="on">
+                <StyleName>raster-style</StyleName>
+                <Datasource>
+                    <Parameter name="type">gdal</Parameter>
+                    <Parameter name="file">cea.tif</Parameter>
+                    <Parameter name="format">tiff</Parameter>
+                </Datasource>
+            </Layer>
+            <Style name="raster-style">
+                <Rule>
+                    <RasterSymbolizer/>
+                </Rule>
+            </Style>
+            </Map>'''
+
+        config_content = {
+            "cache": {
+                "name": "Test",
+                "path": "/tmp/stache",
+                "umask": "0000"
+            },
+            "layers": {
+                "geotiff": {
+                    "provider": {"name": "mapnik", "mapfile": mapnik_config},
+                    "projection": "spherical mercator"
+                }
+            }
+        }
+
+        config = parseConfig(config_content)
+        self.assertTrue(config.layers['geotiff'])
+        self.assertTrue(isinstance(config.layers['geotiff'], Core.Layer))
