e44ce267307130bde004f84e1157c710e851e829
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2016-09-22 10:37:58 -0400

    Made json config readable

--- examples/geotiff/server.py
+++ examples/geotiff/server.py
@@ -42,10 +42,44 @@ if __name__ == '__main__':
     layer_srs = srs.ExportToProj4()
     map_srs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over"
 
-    mapnik_config_json = {"Map": {"@srs": map_srs, "@font-directory": "./fonts", "Style": {"@name": "raster-style", "Rule": {"RasterSymbolizer": {}}},
-                          "Layer": {"@status": "on", "@srs": layer_srs, "@name": "raster-layer",
-                          "StyleName": {"$": "raster-style"}, "Datasource": {"Parameter": [{"@name": "type", "$": "gdal"},
-                          {"@name": "file", "$": filename}, {"@name": "format", "$": "tiff"}]}}}}
+    mapnik_config_json = {
+       "Map":{
+          "@srs":map_srs,
+          "@font-directory":"./fonts",
+          "Style":{
+             "@name":"raster-style",
+             "Rule":{
+                "RasterSymbolizer":{
+
+                }
+             }
+          },
+          "Layer":{
+             "@status":"on",
+             "@srs":layer_srs,
+             "@name":"raster-layer",
+             "StyleName":{
+                "$":"raster-style"
+             },
+             "Datasource":{
+                "Parameter":[
+                   {
+                      "@name":"type",
+                      "$":"gdal"
+                   },
+                   {
+                      "@name":"file",
+                      "$":filename
+                   },
+                   {
+                      "@name":"format",
+                      "$":"tiff"
+                   }
+                ]
+             }
+          }
+       }
+    }
 
 
     mapnik_config = tostring(bf.etree(mapnik_config_json)[0])
