493e93f0f5b15bf0b4aedbcd5bfd7c946372ad17
doruk.ozturk <doruk.ozturk@kitware.com>
2016-10-20 10:16:52 -0400

    Moved instructions to a readme file

--- examples/tornado/server.py
+++ examples/tornado/server.py
@@ -1,11 +1,3 @@
-""" Run this script from command line like "python server.py"
-    To test if it is working either create an 'osm' map example
-    in geojs (https://gist.github.com/dorukozturk/5ab6eceb0202115af468cb5b30523424)
-    or simply hit this url: http://localhost:8080/geotiff/11/356/819.png
-    If you see an image that means it is working.
-"""
-
-
 import os
 
 from osgeo import gdal
